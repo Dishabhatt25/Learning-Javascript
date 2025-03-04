e2ceb1736cd687bff8a51524460caba92d8f10e4
Disha Bhatt <dishab466@gmail.com>
2025-03-04 11:27:32 +0530

    My-first-commit

---
+++ practice.js
@@ -0,0 +1,247 @@
+// console.log("Disha Bhatt");
+
+// x = "BCA";
+// console.log(x);
+
+// y = null;
+// console.log(y);//variable with same name can be updated 
+// //only in paranthesis..not outside it.
+// {
+//   let age = 21
+//   console.log(age)
+// };
+// {
+//   let age = 25;
+//   console.log(age)
+
+// };
+
+// var price = 200;
+// console.log(price);
+
+// var price = 900;
+// console.log(price);
+
+// const marks = 44;
+// console.log(marks);
+
+// //object
+// const student = {
+//   fullName: "Disha Bhatt",
+//   Department: "BCA",
+//   Semester: 6,
+//   cgpa: 8.8,
+// };
+// console.log(student.fullName);
+// student["Semester"] = student["Semester"] + 1;
+// console.log(student.Semester);
+
+
+// //Practice question 1
+// const product = {
+//   Name: "ball pen",
+//   price: 30,
+//   offer: 5,
+//   Rate: 5,
+// };
+// console.log(product);
+
+// //Practice Question 2
+// const profile = {
+//   Username: "@Dishuuu1",
+//   Follower: 150,
+//   Following: 100,
+//   Bio: "om namah shivaay",
+// };
+// console.log(profile);
+
+// // ARITHMETIC OPERATOR
+// let a = 5;
+// let b = 3;
+// console.log("a =5  ", "b = 3 ",);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a * b = ", a * b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+// console.log("++a = ", ++a);
+// console.log("--a = ", --a);
+
+
+// // Conditional statements Questions(ODD/EVEN) 
+// //let Num = prompt("Enter a num");
+// //if(Num%2==0){
+// //  Num = "Even"
+// //}else{
+// //  Num = "Odd"
+// //}
+// //console.log("Given number is", Num);
+
+// //LARGEST NUMBER 
+// let num1 = 34;
+// let num2 = 34;
+// if (num1 > num2) {
+//   console.log("num1 is greater")
+// } else if (num2 > num1) {
+//   console.log("num2 is greater")
+// } else {
+//   console.log("Both num are equal")
+// };
+
+
+// //Statements
+// //Conditional Statement
+// let age = 15;
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+// if (age < 18) {
+//   console.log("you cannot vote");
+// }
+
+// //Example 2
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//   color = "black";
+// }
+// if (mode === "light") {
+//   color = "white";
+// }
+// console.log(color);
+
+// //content related to first video
+// console.log("My name is Disha bhatt");
+// console.log("BCA");
+// FullName = "disha bahtt";
+// console.log("FullName");
+// console.log(FullName);
+
+// age = 24;
+// console.log(age);
+
+// price = 144;
+// console.log(price);
+
+// x = null;
+// console.log(x);
+
+// y = undefined;
+// console.log(y);
+
+// FullName = "bhoomika";
+// console.log(FullName);
+// fullname = "disha bhatt";
+// FULLNAME ="khushi";
+// console.log(fullname);
+// console.log(FULLNAME);
+
+
+
+// let name ="Disha bhatt";
+// console.log(name);
+// var fullName = "Disha Bhatt";
+// console.log(fullName);
+// {
+//     let a = 5;
+//     console.log(a);
+// }
+// {
+//     let a = 6; 
+//     console.log(a);
+// }
+// const student = {
+//     fullName : "Rahul",
+//     age : 21,
+//     cgpa : 9.9,
+// };
+// console.log(student["age"]) + 1;
+
+
+
+
+
+// //loops
+// //for in loops
+// for(let i =1;i<=10;i++){
+//     console.log("i = ", i);//10  times executed 
+//   }
+//   console.log("loop has ended");
+
+//   //sum 1 to 5
+//   let sum = 0;
+//   for (let i=1; i<=5; i++) {
+//       sum = sum + i;
+//   }
+//   console.log("sum = ", sum);
+//   console.log("loop has ended");
+
+//   //Multiply from 1 to 5
+//   let mul =1;
+//   for (let i = 1; i<=5; i++){
+//   mul = mul * i;
+//   }
+//      console.log("Multiply = ", mul);
+//      console.log("Finish");
+
+//   //while loop
+//   let i =1;
+//   while(i<=5){
+//       console.log("i = ", i);
+//       i++;
+//   }
+
+//   //do while loop
+//   let j = 1;
+//   do{
+//       console.log("i = ",i );
+//       j++;
+//   } while (j<=5);
+
+//   //for of loop
+//   let str = "DishaBhatt";
+//   for (let i of str){//iteration
+//       console.log("i=", i);
+//   }
+
+//   let l ="iloveyou";
+//   let size = 0;
+//   for(let i of l){
+//       console.log("i= ",i);
+//       size++;
+//   }
+//   console.log("string size= ",size); 
+
+
+//FOR LOOP 
+//1.Print number 1 to 5
+for (let i = 1; i <= 5; i++) {
+  console.log("i = ", i);
+}
+ console.log("loop has ended");
+
+ //2.Print Your name
+ for(let i = 1; i<=5; i++){
+  console.log("Disha Bhatt");
+ }
+ console.log("Loop has ended");
+
+  //3.Sum of 5 number
+  let sum = 0;
+  for(let i =1; i<=5; i++){
+    sum = sum + i;
+  }
+  console.log("sum = ",sum);
+  console.log("loop has ended");
+
+   
+  //3. Multiply number from 1 to 5
+  let mul =1;
+  for(let i =1; i<=5; i++){
+    mul = mul * i;
+  }
+  console.log("mul = ", mul);
+  console.log("loop has ended");
+
